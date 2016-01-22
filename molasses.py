aa6396451e95bab43e2f3795a4182ac8d220e996
brendon-boldt <brendon_boldt@yahoo.com>
2016-01-22 00:56:32 -0500

    Fixes issue with improper array to byte conversion

--- molasses.py
+++ molasses.py
@@ -2,13 +2,13 @@
 
 import tensorflow as tf
 import gzip
-import numpy
+import numpy as np
 import functools as ft
 
 def _read32(bytestream):
-  dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+  dt = np.dtype(np.uint32).newbyteorder('>')
   buf = bytestream.read(4)
-  return numpy.frombuffer(buf, dtype=dt)[0]
+  return np.frombuffer(buf, dtype=dt)[0]
 
 # dtype doesn't actually do anything yet
 def tensor_to_idx(var, filename, dtype='float'):
@@ -18,11 +18,11 @@ def idx_to_tensor(filename):
   return tf.constant(idx_to_array(filename))
 
 def array_to_idx(var, filename, dtype='f4'):
-  data = numpy.reshape(var,[-1]).astype(dtype)
-  dt = numpy.dtype('u4').newbyteorder('>')
-  shape = numpy.array(var.shape, dtype=dt)
+  data = np.reshape(var,[-1]).astype(dtype)
+  dt = np.dtype('u4').newbyteorder('>')
+  shape = np.array(var.shape, dtype=dt)
   with gzip.open(filename, 'wb') as f:
-    f.write(bytes([0, 0, 0xd, len(shape)]))
+    f.write(str(bytearray([0, 0, 0xd, len(shape)])))
     f.write(shape.tostring()) # Was tobytes
     f.write(data.tostring())
 
@@ -32,11 +32,11 @@ def idx_to_array(filename):
     # magic & 0xff == number of dimensions
     shape = []
     # dt will need to be changed eventually
-    dt = numpy.dtype('f4')
+    dt = np.dtype('f4')
     for i in range(magic & 0xff):
       shape.append(_read32(bytestream))
     buf = bytestream.read(dt.itemsize * ft.reduce(lambda x,y: x*y, shape))
-    data = numpy.frombuffer(buf, dtype=dt)
+    data = np.frombuffer(buf, dtype=dt)
     array = data.tolist()
-    array = numpy.reshape(array, shape).astype(dt)
+    array = np.reshape(array, shape).astype(dt)
     return array
